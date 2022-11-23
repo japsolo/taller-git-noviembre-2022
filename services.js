cef4a13d98297c9ae6e5e5e1cdef792115942b86
javi-teaches <japsolo@gmail.com>
2022-11-23 09:07:19 -0500

    userName added

--- services.js
+++ services.js
@@ -1,5 +1,7 @@
+const userName = "Jane";
 console.info("Services");
 console.info("Services");
 console.info("Services");
 console.info("Services");
 console.info("Services");
+console.log(userName);
